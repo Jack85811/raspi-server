1c969c33c9dc14b688fd891b01c4b7d594e21a6d
jack85811 <jack-sl@web.de>
2017-04-16 14:41:39 +0200

    'server'

---
+++ raspiServer.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var sudo = require('sudo-prompt');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+
+// JSON API
+app.post('/switchon',function (req,res){
+    console.log(req.body);
+    var systemCode = req.body.systemCode;
+    var socketNumber = req.body.socketNumber;
+    var status = req.body.status;
+    var options = {
+      name: 'Electron'
+    };
+    sudo.exec('sudo raspberry-remote/send '+systemCode+ ' '+socketNumber+ ' ' + status, options, function(error, stdout, stderr) {});
+    res.send("ok");
+
+});
+
+// Start server
+var port= 8081;
+app.listen(port);
+console.log("Server running at http://localhost:" + port );
