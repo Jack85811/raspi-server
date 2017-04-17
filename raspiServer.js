71158b8b5b3ee2b7760392b559287759ea17559c
jack85811 <jack-sl@web.de>
2017-04-17 11:43:15 +0200

    :

--- raspiServer.js
+++ raspiServer.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-}):
+});
 
 
 // JSON API
