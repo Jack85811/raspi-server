efa39f93f54fce2709fbfe6467de8be1d5ee7fe0
jack85811 <jack-sl@web.de>
2017-04-17 21:20:14 +0200

    switchall

--- raspiServer.js
+++ raspiServer.js
@@ -61,7 +61,7 @@ app.post('/switchalloff',function (req,res){
         executedString+=jsonPart.systemCode + " "+ jsonPart.socketNumber + " ";
     });
 
-    executeSudoSend(executeString + " " + status);
+    executeSudoSend(executeString + " " + status, res);
 });
 
 function executeSudoSend(string,res){
