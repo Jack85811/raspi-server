356841fde3422937bbe7bafed1f0087c57b0dfcd
jack85811 <jack-sl@web.de>
2017-07-04 20:14:23 +0200

    change

--- raspiServer.js
+++ raspiServer.js
@@ -8,24 +8,6 @@ var child;
 app.use(queue({ activeLimit: 1 }));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 
-var rpi433    = require('rpi-433'),
-    rfSniffer = rpi433.sniffer({
-      pin: 27,                     //Snif on GPIO 2 (or Physical PIN 13)
-      debounceDelay: 500          //Wait 500ms before reading another code
-    }),
-    rfEmitter = rpi433.emitter({
-      pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
-      pulseLength: 350            //Send the code with a 350 pulse length
-    });
-
-// Receive (data is like {code: xxx, pulseLength: xxx})
-rfSniffer.on('data', function (data) {
-  console.log('Code received: '+data.code+' pulse length : '+data.pulseLength);
-  if (data.equals("70929")) {
-    console.log("hola");
-  }
-});
-
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
